10d221e91e5f3b8f2b280cfd63871497da42960a
Surya <suryakant.subudhi@outlook.com>
2023-08-08 21:00:29 +0530

    config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    "no-unused-modules": "off"
+    "no-unused-modules": "off",
+    "no-unused-vars": "off",
   },
 }
